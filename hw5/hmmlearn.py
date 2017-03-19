20db9b562cf8bc70788191b0c42f92655a2f070a
Elliscope Fang <mingzhef@usc.edu>
2017-03-19 13:38:33 -0700

    darft transition model completed

--- hw5/hmmlearn.py
+++ hw5/hmmlearn.py
@@ -16,9 +16,12 @@ def line_parsing(line):
   index = index + 1
   if transi_dic.has_key(str(initial_tag[1])) == False:
     transi_dic[str(initial_tag[1])] = {}
+    #initialize the count to be 1 
+    transi_dic[str(initial_tag[1])]['count'] = 1.0
   else:
+    transi_dic[str(initial_tag[1])]['count'] = transi_dic[str(initial_tag[1])]['count'] + 1
     if transi_dic[str(initial_tag[1])].has_key(end_tag[1]) == False:
-        transi_dic[str(initial_tag[1])][end_tag[1]] = 1
+        transi_dic[str(initial_tag[1])][end_tag[1]] = 1.0
     else:
         transi_dic[str(initial_tag[1])][end_tag[1]] = transi_dic[str(initial_tag[1])][end_tag[1]] + 1
 
@@ -27,8 +30,30 @@ with open(sys.argv[1]) as ft:
     for line in ft:
         line_parsing(line)
 
+
+
+# start_tag = 0
+# while start_tag < len(transi_dic):
+#   for end_tag in transi_dic[start_tag][]
+
+transi_pro_dic = {}
+
+
+for key,value in transi_dic.items():
+  print (key,value)
+  count = 1.0
+  transi_pro_dic[key] = {}
+  if type(value) is dict:
+    for k,v in value.items():
+      if value.has_key('count'):
+        v = v/value['count']
+        if k != 'count':
+          transi_pro_dic[key][k] = v
+
+print transi_pro_dic
+
 output_json_data = {}
-output_json_data['transi_dic'] = transi_dic
+output_json_data['transi_pro_dic'] = transi_pro_dic
 
 with open('hmmmodel.txt', 'w') as outfile:
     json.dump(output_json_data,outfile, sort_keys = True, indent = 4,ensure_ascii=False)
